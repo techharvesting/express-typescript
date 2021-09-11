9de6c8575920c317f239c3487dabc60d3e18f9c8
naseelniyas <naseel@techharvesting.in>
2021-09-11 11:09:45 +0530

    👍 Added required files

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express, {Application, Request, Response} from 'express'
+
+const app: Application = express()
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello!')
+})
+
+app.listen(3000)
